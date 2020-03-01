51e0bb94848c18b384e0f9284ecd7be6b5ce6707
root <marvinnitz18@gmail.com>
2020-03-01 18:16:16 +0100

    first commit

---
+++ map.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.7
+import json
+import folium
+m = folium.Map(location=[38, 6], tiles='Stamen Toner', zoom_start=2)
+
+coordinates = open("/root/scripts/coordinate.list", "r")
+line = coordinates.readline()
+user_count= 1
+while line:
+    line = line.replace('\n','')
+    line = eval(line)
+    lat = line['lat']
+    long = line['long']
+    folium.Marker([lat, long], tooltip="User #"+str(user_count)).add_to(m)
+    user_count= user_count + 1
+    line = coordinates.readline()
+coordinates.close()
+m.save('/var/www/html/map.html')
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/usr/bin/python /root/scripts/map.py
+touch /root/scripts/lol
